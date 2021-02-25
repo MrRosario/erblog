877857d96172d07c58f07dbc687ecaa7f5a0c33b
MrRosario <eugenio.dorosario94@gmail.com>
2021-02-25 18:17:28 -0300

    chore: SEO

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,24 @@
-import React from 'react';
 import Link from 'next/link';
+import Seo from '../components/Seo';
 
  const NotFOund = () => {
 
   return(
-      <div className="notFound">
+    <>
+      <Seo 
+        title='Página não encontrada || Eugenio Rosario'
+        description='Blog e Site pessoal de um desenvolvedor software apaixonado por desenvolver sistemas, e onde compartilha seus conhecimentos sobre programação.'
+        currentPage='https://eugeniorosario.com/404'
+      />
+      <div className="not-found">
         <h2>
-            Pagina não encontrada, por favor volte para 
-            <Link href="/">
-                <a> Home</a>
-            </Link>
+          Pagina não encontrada, por favor volte para 
+          <Link href="/">
+              <a> Home</a>
+          </Link>
         </h2>
       </div>
+    </>
   ) 
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,62 @@
 import Card from '../components/Card';
 import useFetchApi from '../hooks/useFetchApi';
 import Link from 'next/link';
+import Seo from '../components/Seo';
 
  const Home = ({ Blogs }) => {
   return (
-    <div className="main">
-      <div className="content">
-        <article className="introduction">
-          <h1>Olá</h1>
-          <p>
-            Meu nome é Eugénio, apaixonado por desenvolvimento de software,  
-            sustentabilidade, e tudo relacionado a tecnologia.
-            Acredito que posso melhorar o mundo desenvolvendo coisas.
-          </p>
-          <p> 
-            Tenho um  
-            <a 
-              href="https://www.youtube.com/channel/UCA4uTaYYxjrvXNvs-SO8HSg" 
-              rel="noopener" 
-              target="_blank"
-              className="btn btn-view-more"
-            >
-              canal no youtube com conteúdos sobre programação.
-            </a>
-          </p>
-        </article>
-        <section className='recent-posts'>
-          <h3>Publicações recentes</h3>
+    <>
+      <Seo 
+        title='Página Inicial || Eugenio Rosario'
+        description='Blog e Site pessoal de um desenvolvedor software apaixonado por desenvolver sistemas, e onde compartilha seus conhecimentos sobre programação.'
+        currentPage='https://eugeniorosario.com'
+      />
+      <div className="main">
+        <div className="content">
+          <article className="introduction">
+            <h1>Olá</h1>
+            <p>
+              Meu nome é Eugénio, apaixonado por desenvolvimento de software,  
+              sustentabilidade, e tudo relacionado a tecnologia.
+              Acredito que posso melhorar o mundo desenvolvendo coisas.
+            </p>
+            <p> 
+              Tenho um  
+              <a 
+                href="https://www.youtube.com/channel/UCA4uTaYYxjrvXNvs-SO8HSg" 
+                rel="noopener" 
+                target="_blank"
+                className="btn btn-view-more"
+              >
+                canal no youtube com conteúdos sobre programação.
+              </a>
+            </p>
+          </article>
+          <section className='recent-posts'>
+            <h3>Publicações recentes</h3>
 
-          { Blogs.slice(0,2).map( item => (
-              <Card 
-                PostTitle={item.title}
-                PostDate={item.published_at}
-                PostDecription={item.description}
-                key={item.id}
-                PostSlug={item.slug}
-                PostId={item.id}
-                PostFeaturedImg={item.featured_image}
-              />
-            )
-          )}
-          <Link href="/blog">
-            <a className="btn btn-view-more">
-              Ver todos os posts
-            </a>
-          </Link>
+            { Blogs.slice(0,2).map( item => (
+                <Card 
+                  PostTitle={item.title}
+                  PostDate={item.published_at}
+                  PostDecription={item.description}
+                  key={item.id}
+                  PostSlug={item.slug}
+                  PostId={item.id}
+                  PostFeaturedImg={item.featured_image}
+                />
+              )
+            )}
+            <Link href="/blog">
+              <a className="btn btn-view-more">
+                Ver todos os posts
+              </a>
+            </Link>
 
-        </section>
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
