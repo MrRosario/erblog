c644559634d2030dfb41f274e68736712514f703
MrRosario <eugenio.dorosario94@gmail.com>
2021-02-25 18:54:29 -0300

    chore: tags

--- components/Seo.js
+++ components/Seo.js
@@ -15,18 +15,18 @@ const Seo = ({ title, description, currentPage }) => (
     <meta name="og:description" property="og:description" content={description} />
     <meta property="og:site_name" content="Eugenio Rosario || Web e Mobile" />
     <meta property="og:url" content={currentPage} />  
-    <meta property="og:image" content="/logo.png" />  
-    <meta name="twitter:image" content="/logo.png" />  
+    <meta property="og:image" content="/eugeniorosario_ico.png" />  
+    <meta name="twitter:image" content="/eugeniorosario_ico.png" />  
     <meta name="twitter:card" content="summary" /> 
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
     <meta name="twitter:site" content={currentPage} />
-    <link rel="icon" type="image/png" href="/logo.png" />
-    <link rel="apple-touch-icon" href="/logo.png" />
-    <link rel="shortcut icon" href="/logo.png"/>
-    <link rel="icon" href="/logo.png"/>
-    <link rel="apple-touch-icon" href="/logo.png" />
-    <link rel="apple-touch-startup-image" href="/logo.png" />
+    <link rel="icon" type="image/png" href="/eugeniorosario_ico.png" />
+    <link rel="apple-touch-icon" href="/eugeniorosario_ico.png" />
+    <link rel="shortcut icon" href="/eugeniorosario_ico.png"/>
+    <link rel="icon" href="/eugeniorosario_ico.png"/>
+    <link rel="apple-touch-icon" href="/eugeniorosario_ico.png" />
+    <link rel="apple-touch-startup-image" href="/eugeniorosario_ico.png" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
     <meta name="msapplication-navbutton-color" content="#0077b6" /> 
