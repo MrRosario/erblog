f933caf672ac179f23e2007222238f9b71ae72da
MrRosario <eugenio.dorosario94@gmail.com>
2021-02-26 12:24:54 -0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+
+  }, [router.events])
+
   return(
     <Layout>
       <Component {...pageProps} />
